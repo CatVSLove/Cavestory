300fc08d74935843d0d0da9debe820596f3b0414
CatVSLove2d <118850264+CatVSLove@users.noreply.github.com>
2024-06-01 20:00:55 -0500

    Add files via upload

---
+++ Cavestory-20Years-Later/src/tools/misc/variables.lua
@@ -0,0 +1,13 @@
+--Random variables
+
+function MakeVariables()
+  gravity = 400
+  scale = 4
+
+  playstate = 1
+  titlestate = 2
+
+  gamestate = titlestate
+
+  fontMain = love.graphics.newFont('fonts/CaveStory.ttf', 40)
+end
